7037f6dacb9c8ec28135411fcac3d8253e8cf476
Sandesh Sukubhattu <sukubhattusandesh8@gmail.com>
2020-07-24 16:31:10 +0545

    Add file manager components

---
+++ src/components/SideNav.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+/* Leaving as stateful or class components
+  Initially this code is just UI component.
+  Further it can be changed
+*/
+class SideNav extends Component {
+  render() {
+    return <div>Hello I am sidenav</div>;
+  }
+}
+
+export default SideNav;
