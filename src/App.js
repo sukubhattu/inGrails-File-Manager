1749822baa3c56c9e05abd7469f4b86de9a25799
Sandesh Sukubhattu <sukubhattusandesh8@gmail.com>
2020-07-26 19:23:08 +0545

    Update home screen UI

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ import "./css/reset.css";
 import "./css/style.css";
 
 // Importing Fonts
-// import "./fonts/QaneleasLight.otf";
-// import "./fonts/QaneleasBold.otf";
+import "./fonts/SFPro.ttf";
 
 // Importing components
 import MenuNav from "./components/menuNav/MenuNav";
