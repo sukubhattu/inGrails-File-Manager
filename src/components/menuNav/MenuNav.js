4fd6e253ef898fc22f7638370212e0955cf077c6
Sandesh Sukubhattu <sukubhattusandesh8@gmail.com>
2020-07-26 13:53:20 +0545

    Update menu nav to stateless component

--- src/components/menuNav/MenuNav.js
+++ src/components/menuNav/MenuNav.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 // Importing CSS
 import "./MenuNav.css";
@@ -12,55 +12,53 @@ import { FiMessageSquare } from "react-icons/fi";
 import { AiOutlineUser, AiFillSetting } from "react-icons/ai";
 import { RiFileChartLine } from "react-icons/ri";
 
-/* Leaving as stateful or class components
+/* Leaving as stateless or function component
   Initially this code is just UI component.
   Further it can be changed
 */
-class MenuNav extends Component {
-  render() {
-    return (
-      <div className="menu-nav">
-        <div className="menu-nav-logo">
-          <img className="menu-nav-logo-img" src={logo} alt="user" />
-        </div>
-
-        <ul className="menu-nav-icons">
-          <li>
-            <a href="#" title="Copy">
-              <MdContentCopy size={24} />
-            </a>
-          </li>
-          <li>
-            <a href="#" title="Messages">
-              <FiMessageSquare size={24} />
-            </a>
-          </li>
-          <li>
-            <a href="#" title="Group">
-              <AiOutlineUser size={24} />
-            </a>
-          </li>
-          <li>
-            <a href="#" title="Chart">
-              <RiFileChartLine size={24} />
-            </a>
-          </li>
-          <li>
-            <a href="#" title="Setting">
-              <AiFillSetting size={24} />
-            </a>
-          </li>
-        </ul>
-        <div className="user-image">
-          <img
-            className="user-img-lg"
-            src="https://picsum.photos/40/40"
-            alt="user"
-          />
-        </div>
+function MenuNav() {
+  return (
+    <div className="menu-nav">
+      <div className="menu-nav-logo">
+        <img className="menu-nav-logo-img" src={logo} alt="user" />
       </div>
-    );
-  }
+
+      <ul className="menu-nav-icons">
+        <li>
+          <a href="#" title="Copy">
+            <MdContentCopy size={24} />
+          </a>
+        </li>
+        <li>
+          <a href="#" title="Messages">
+            <FiMessageSquare size={24} />
+          </a>
+        </li>
+        <li>
+          <a href="#" title="Group">
+            <AiOutlineUser size={24} />
+          </a>
+        </li>
+        <li>
+          <a href="#" title="Chart">
+            <RiFileChartLine size={24} />
+          </a>
+        </li>
+        <li>
+          <a href="#" title="Setting">
+            <AiFillSetting size={24} />
+          </a>
+        </li>
+      </ul>
+      <div className="user-image">
+        <img
+          className="user-img-lg"
+          src="https://picsum.photos/40/40"
+          alt="user"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default MenuNav;
