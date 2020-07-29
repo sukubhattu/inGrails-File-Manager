4696aa14d4acb27b4b761e85f81ee9ca78f979a1
Sandesh Sukubhattu <sukubhattusandesh8@gmail.com>
2020-07-29 19:59:07 +0545

    Add cloud file dummy data

---
+++ src/components/mainContainer/cloud-files.js
@@ -0,0 +1,28 @@
+// import Image
+import googleDrive from "./img/google-drive.png";
+import oneDrive from "./img/onedrive.png";
+import dropBox from "./img/dropbox.png";
+
+export default [
+  {
+    icon: googleDrive,
+    name: "Google Drive",
+    used: "45 GB",
+    total: "50 GB",
+    percentage: 90,
+  },
+  {
+    icon: oneDrive,
+    name: "One Drive",
+    used: "1.2 GB",
+    total: "3 GB",
+    percentage: 40,
+  },
+  {
+    icon: dropBox,
+    name: "Drop Box",
+    used: "2.5 GB",
+    total: "5 GB",
+    percentage: 50,
+  },
+];
