6acb4f1e9ea971c9f52a9edbbb41cc28b0cd8c76
Sandesh Sukubhattu <sukubhattusandesh8@gmail.com>
2020-07-29 20:00:43 +0545

    Update dynammic card for cloud storage

--- src/components/mainContainer/MainContainer.js
+++ src/components/mainContainer/MainContainer.js
@@ -6,93 +6,14 @@ import "./MainContainer.css";
 // Import router
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
-// import Image
-import googleDrive from "./img/google-drive.png";
-import oneDrive from "./img/onedrive.png";
-import dropBox from "./img/dropbox.png";
-
 // Import icons
 import { MdMoreHoriz } from "react-icons/md";
 import { AiOutlineArrowUp } from "react-icons/ai";
 import { FaFolder } from "react-icons/fa";
 
-// Import JSON
+// Import dummy data variables
 import files from "./user-files";
-
-// Make a folder template for show all and hide all
-const folderTemplate = (
-  <div>
-    <div className="card">
-      <div className="shared-folder-card">
-        <div className="random-name clearfix">
-          <div className="float-left">
-            <i className="shared-folder-card-info-name">
-              <FaFolder size={32} />
-            </i>
-            <h4>Analytics</h4>
-            <p>15 Files</p>
-          </div>
-          <div className="float-right">
-            <div className="shared-people-1">
-              <img
-                className="shared-people-img1"
-                src="https://picsum.photos/20/20"
-                alt="shared user"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="shared-folder-card second-card">
-        <div className=" clearfix">
-          <div className="float-left">
-            <i className="shared-folder-card-info-name">
-              <FaFolder size={32} />
-            </i>
-            <h4>Assets</h4>
-            <p>345 Files</p>
-          </div>
-          <div className="float-right">
-            <div className="shared-people-1">
-              <img
-                className="shared-people-img1"
-                src="https://picsum.photos/20/20"
-                alt="shared user"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="shared-folder-card">
-        <div className=" clearfix">
-          <div className="float-left">
-            <i className="shared-folder-card-info-name">
-              <FaFolder size={32} />
-            </i>
-            <h4>Marketing</h4>
-            <p>143 Files</p>
-          </div>
-          <div className="float-right">
-            <div className="shared-people-1">
-              <img
-                className="shared-people-img1"
-                src="https://picsum.photos/20/20"
-                alt="shared user"
-              />
-            </div>
-            <div className="shared-people-2">
-              <img
-                className="shared-people-img2"
-                src="https://picsum.photos/20/20"
-                alt="shared user"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+import cloudFiles from "./cloud-files";
 
 /* Making this component as class or stateful component for view all button demo purpose */
 class MainContainer extends Component {
@@ -123,70 +44,37 @@ class MainContainer extends Component {
           {/* search bar ends here */}
 
           {/* cloud storage card starts here */}
+
+          {/* 
+            DYNAMIC card without hard coding for cloud storage
+           */}
           <div className="card">
-            <div className="cloud-storage-card">
-              <div className="cloud-storage-name clearfix">
-                <div className="float-left">
-                  <img src={googleDrive} alt="Google Drive" />
-                  <br />
-                  <h4> Google Drive</h4>
+            {cloudFiles.map((file, index) => (
+              <div className="cloud-storage-card">
+                <div className="cloud-storage-name clearfix">
+                  <div className="float-left">
+                    <img src={file.icon} alt="Google Drive" />
+                    <br />
+                    <h4> {file.name}</h4>
+                  </div>
+                  <div className="float-right">
+                    <i className="cloud-storage-name-value">
+                      <MdMoreHoriz size={24} />
+                    </i>
+                  </div>
                 </div>
-                <div className="float-right">
-                  <i className="cloud-storage-name-value">
-                    <MdMoreHoriz size={24} />
-                  </i>
+                <div className="cloud-storage-amount clearfix">
+                  <p className="float-left">{file.used}</p>
+                  <p className="float-right">{file.total}</p>
                 </div>
-              </div>
-              <div className="cloud-storage-amount clearfix">
-                <p className="float-left">45 GB</p>
-                <p className="float-right">50 GB</p>
-              </div>
-              <div className="storage-progress-bar-individual">
-                <div className="storage-progress-bar-individual1"></div>
-              </div>
-            </div>
-            <div className="cloud-storage-card second-card">
-              <div className="cloud-storage-name clearfix">
-                <div className="float-left">
-                  <img src={dropBox} alt="Drop Box" />
-                  <br />
-                  <h4>Dropbox</h4>
-                </div>
-                <div className="float-right">
-                  <i className="cloud-storage-name-value">
-                    <MdMoreHoriz size={24} />
-                  </i>
+                <div className="storage-progress-bar-individual">
+                  <div
+                    className="storage-progress-bar-individual1"
+                    style={{ width: file.percentage + "%" }}
+                  ></div>
                 </div>
               </div>
-              <div className="cloud-storage-amount clearfix">
-                <p className="float-left">45 GB</p>
-                <p className="float-right">50 GB</p>
-              </div>
-              <div className="storage-progress-bar-individual">
-                <div className="storage-progress-bar-individual1"></div>
-              </div>
-            </div>
-            <div className="cloud-storage-card">
-              <div className="cloud-storage-name clearfix">
-                <div className="float-left">
-                  <img src={oneDrive} alt="One Drive" />
-                  <br />
-                  <h4>One Drive</h4>
-                </div>
-                <div className="float-right">
-                  <i className="cloud-storage-name-value">
-                    <MdMoreHoriz size={24} />
-                  </i>
-                </div>
-              </div>
-              <div className="cloud-storage-amount clearfix">
-                <p className="float-left">45 GB</p>
-                <p className="float-right">50 GB</p>
-              </div>
-              <div className="storage-progress-bar-individual">
-                <div className="storage-progress-bar-individual1"></div>
-              </div>
-            </div>
+            ))}
           </div>
           {/* cloud storage card ends here */}
 
@@ -201,7 +89,7 @@ class MainContainer extends Component {
                 className="folder-view-all"
                 onClick={this.toggleShowFolder}
               >
-                {this.state.showingFolder ? "Hide All" : "View All"}
+                {this.state.showingFolder ? "Hide" : "View All"}
               </Link>
             </div>
           </div>
@@ -265,4 +153,82 @@ class MainContainer extends Component {
   }
 }
 
+/* 
+  This folderTemplate is for show all and hide option.
+  This card is hard coded.
+  DYNAMIC version of same card is done in cloud storage card section.
+*/
+const folderTemplate = (
+  <div>
+    <div className="card">
+      <div className="shared-folder-card">
+        <div className="random-name clearfix">
+          <div className="float-left">
+            <i className="shared-folder-card-info-name">
+              <FaFolder size={32} />
+            </i>
+            <h4>Analytics</h4>
+            <p>15 Files</p>
+          </div>
+          <div className="float-right">
+            <div className="shared-people-1">
+              <img
+                className="shared-people-img1"
+                src="https://picsum.photos/20/20"
+                alt="shared user"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="shared-folder-card">
+        <div className=" clearfix">
+          <div className="float-left">
+            <i className="shared-folder-card-info-name">
+              <FaFolder size={32} />
+            </i>
+            <h4>Assets</h4>
+            <p>345 Files</p>
+          </div>
+          <div className="float-right">
+            <div className="shared-people-1">
+              <img
+                className="shared-people-img1"
+                src="https://picsum.photos/20/20"
+                alt="shared user"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="shared-folder-card">
+        <div className=" clearfix">
+          <div className="float-left">
+            <i className="shared-folder-card-info-name">
+              <FaFolder size={32} />
+            </i>
+            <h4>Marketing</h4>
+            <p>143 Files</p>
+          </div>
+          <div className="float-right">
+            <div className="shared-people-1">
+              <img
+                className="shared-people-img1"
+                src="https://picsum.photos/20/20"
+                alt="shared user"
+              />
+            </div>
+            <div className="shared-people-2">
+              <img
+                className="shared-people-img2"
+                src="https://picsum.photos/20/20"
+                alt="shared user"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 export default MainContainer;
