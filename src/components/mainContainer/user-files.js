365b2d0354fc7d917e88d8ec5537993319002597
Sandesh Sukubhattu <sukubhattusandesh8@gmail.com>
2020-07-29 18:44:47 +0545

    Add user file json object

---
+++ src/components/mainContainer/user-files.js
@@ -0,0 +1,67 @@
+// Use this file to stop rewriting multiple tr in MainContainer.js
+
+import pdfIcon from "./img/pdf.png";
+import zipIcon from "./img/zip-format.png";
+import docIcon from "./img/doc.png";
+export default [
+  {
+    icon: pdfIcon,
+    name: "This is random file name",
+    sharedWith: "only you",
+    date: "30 Sept",
+  },
+  {
+    icon: docIcon,
+    name: "This is random file name",
+    sharedWith: "only you",
+    date: "30 Sept",
+  },
+  {
+    icon: zipIcon,
+    name: "This is random file name",
+    sharedWith: "only you",
+    date: "30 Sept",
+  },
+  {
+    icon: docIcon,
+    name: "This is random file name",
+    sharedWith: "only you",
+    date: "30 Sept",
+  },
+  {
+    icon: pdfIcon,
+    name: "This is random file name",
+    sharedWith: "only you",
+    date: "30 Sept",
+  },
+  {
+    icon: pdfIcon,
+    name: "This is random file name",
+    sharedWith: "only you",
+    date: "30 Sept",
+  },
+  {
+    icon: docIcon,
+    name: "This is random file name",
+    sharedWith: "only you",
+    date: "30 Sept",
+  },
+  {
+    icon: zipIcon,
+    name: "This is random file name",
+    sharedWith: "only you",
+    date: "30 Sept",
+  },
+  {
+    icon: docIcon,
+    name: "This is random file name",
+    sharedWith: "only you",
+    date: "30 Sept",
+  },
+  {
+    icon: pdfIcon,
+    name: "This is random file name",
+    sharedWith: "only you",
+    date: "30 Sept",
+  },
+];
