d11957de787f11957768163a3433b6fa739e3c7d
Sandesh Sukubhattu <sukubhattusandesh8@gmail.com>
2020-07-30 21:19:51 +0545

    Add storage toggler

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 // Importing  App CSS
 import "./App.css";
@@ -16,25 +16,60 @@ import SideNav from "./components/sideNav/SideNav";
 import MainContainer from "./components/mainContainer/MainContainer";
 import Storage from "./components/storage/Storage";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="main-wrapper">
-        <div className="menu-nav-wrapper">
-          <MenuNav />
-        </div>
-        <div className="side-nav-wrapper">
-          <SideNav />
-        </div>
-        <div className="main-container-wrapper">
-          <MainContainer />
-        </div>
-        <div className="storage-wrapper">
-          <Storage />
+class App extends Component {
+  state = {
+    buttonON: true,
+  };
+
+  changeButton = (isButtonOn) => {
+    if (isButtonOn) {
+      this.setState({
+        buttonON: true,
+      });
+    } else {
+      this.setState({
+        buttonON: false,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="main-wrapper">
+          <div className="menu-nav-wrapper">
+            <MenuNav
+              changeButtonFunction={this.changeButton}
+              buttonON={this.state.buttonON}
+            />
+          </div>
+          <div className="side-nav-wrapper">
+            <SideNav />
+          </div>
+
+          <div
+            className={
+              this.state.buttonON
+                ? "main-container-wrapper"
+                : "main-container-wrapper-without-storage"
+            }
+          >
+            <MainContainer />
+          </div>
+
+          <div
+            className={
+              this.state.buttonON
+                ? " storage-wrapper "
+                : "storage-wrapper-nodisplay"
+            }
+          >
+            <Storage />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
